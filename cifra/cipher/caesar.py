c6f90060906e9ff5beb8a8a5b8af0a880e5377d4
dante-signal31 <dante.signal31@gmail.com>
2019-12-25 21:42:32 +0100

    Extracted code to get new char position.

--- cifra/cipher/caesar.py
+++ cifra/cipher/caesar.py
@@ -16,10 +16,10 @@ def cipher(text: str, key: int, charset: str = DEFAULT_CHARSET) -> str:
 
     :param text: Text to be ciphered.
     :param key: Secret key. In Caesar method it corresponds with how many position
-    advance in the charset. Both ends should know this and use the same one.
+     advance in the charset. Both ends should know this and use the same one.
     :param charset: Charset used for Caesar method substitution. Both ends, ciphering
-    and deciphering, should use the same charset or original text won't be properly
-    recovered.
+     and deciphering, should use the same charset or original text won't be properly
+     recovered.
     :return: Ciphered text.
     """
     ciphered_text = _offset_text(text, key, True)
@@ -34,8 +34,8 @@ def decipher(ciphered_text: str, key: int, charset: str = DEFAULT_CHARSET) -> st
 
     :param ciphered_text: Text to be deciphered.
     :param key: Secret key. In Caesar method, and for deciphering end, it correspond
-    with how many position get bat in the charset. Both ends should know this and
-    use the same one.
+     with how many position get bat in the charset. Both ends should know this and
+     use the same one.
     :param charset: Charset used for Caesar method substitutions. Both end should
     use the same charset or original text won't be properly recovered.
     :return: Deciphered text.
@@ -61,15 +61,39 @@ def _offset_text(text: str, key: int, advance: bool, charset: str = DEFAULT_CHAR
         normalized_char = char.lower()
         if normalized_char in charset:
             char_position = charset.index(normalized_char)
-            if advance:
-                new_char_position = (char_position + key) % charset_length
-            else:
-                offset_position = char_position - key
-                new_char_position = offset_position \
-                    if offset_position >= 0 \
-                    else charset_length - (abs(offset_position) % charset_length)
+            # if advance:
+            #     new_char_position = (char_position + key) % charset_length
+            # else:
+            #     offset_position = char_position - key
+            #     new_char_position = offset_position \
+            #         if offset_position >= 0 \
+            #         else charset_length - (abs(offset_position) % charset_length)
+            new_char_position = _get_new_char_position(normalized_char, key, advance, charset)
             new_char = charset[new_char_position] \
                 if char.islower() \
                 else charset[new_char_position].upper()
         offset_text = "".join([offset_text, new_char])
     return offset_text
+
+
+def _get_new_char_position(char: str, key: int, advance: bool, charset=DEFAULT_CHARSET) -> int:
+    """
+    Get position for offset char.
+
+    :param char: Actual character with no offset. It should be normalized to be
+     sure it is present at charset.
+    :param key: Offset to apply.
+    :param advance: If True offset is going to be applied frontwards.
+    :param charset: Charset to use for substitution.
+    :return: Index in charset for offset char.
+    """
+    charset_length = len(charset)
+    char_position = charset.index(char)
+    if advance:
+        new_char_position = (char_position + key) % charset_length
+    else:
+        offset_position = char_position - key
+        new_char_position = offset_position \
+            if offset_position >= 0 \
+            else charset_length - (abs(offset_position) % charset_length)
+    return new_char_position
