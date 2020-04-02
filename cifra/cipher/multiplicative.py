698a5813c31a2ab0be6dd367db89c3b8e7f7b4db
dante-signal31 <dante.signal31@gmail.com>
2020-04-02 19:39:08 +0200

    Multiplicative cipher scaffolding ready.

---
+++ cifra/cipher/multiplicative.py
@@ -0,0 +1,42 @@
+"""
+Library to cipher and decipher texts using Multiplicative method.
+"""
+from cifra.cipher.caesar import DEFAULT_CHARSET, _get_new_char_position, _offset_text
+
+
+def cipher(text: str, key: int, charset: str = DEFAULT_CHARSET) -> str:
+    """
+    Cipher given text using Multiplicative method.
+
+    Be aware that different languages use different charsets. Default charset
+    is for english language, if you are using any other you should use a proper
+    dataset. For instance, if you are ciphering an spanish text, you should use
+    a charset with "ñ" character.
+
+    :param text: Text to be ciphered.
+    :param key: Secret key. In Multiplicative method it corresponds with how many position
+     advance in the charset, taking in count you multiply char index by key. Both ends
+     should know this and use the same one.
+    :param charset: Charset used for Caesar method substitution. Both ends, ciphering
+     and deciphering, should use the same charset or original text won't be properly
+     recovered.
+    :return: Ciphered text.
+    """
+    raise NotImplementedError
+
+
+def decipher(ciphered_text: str, key: int, charset: str = DEFAULT_CHARSET) -> str:
+    """
+    Decipher given text using Multiplicative method.
+
+    Note you should use the same charset that ciphering end did.
+
+    :param ciphered_text: Text to be deciphered.
+    :param key: Secret key. In Multiplicative method it corresponds with how many position
+     advance in the charset, taking in count you multiply char index by key. Both ends
+     should know this and use the same one.
+    :param charset: Charset used for Caesar method substitutions. Both end should
+    use the same charset or original text won't be properly recovered.
+    :return: Deciphered text.
+    """
+    raise NotImplementedError
