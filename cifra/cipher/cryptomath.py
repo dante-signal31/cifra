947e00f16ef1ba1deaf3ea53f78f5e2b2998523b
dante-signal31 <dante.signal31@gmail.com>
2020-04-17 21:14:55 +0200

    Small refactor to ease debugging.

--- cifra/cipher/cryptomath.py
+++ cifra/cipher/cryptomath.py
@@ -33,5 +33,6 @@ def find_mod_inverse(a: int, m: int) -> Optional[int]:
         while v3 != 0:
             q = u3 // v3  # Note that // is the integer division operator.
             v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
-        return u1 % m
+        result = u1 % m
+        return result
 
