3651450ea19afe03c6aac3cdab14db13a9244325
dante-signal31 <dante.signal31@gmail.com>
2021-08-23 20:55:22 +0200

    Untested implementation for ciphering and deciphering at launcher.

--- cifra/cifra_launcher.py
+++ cifra/cifra_launcher.py
@@ -10,24 +10,88 @@
  performs crypto attacks against those methods. I'm implementing this while
  I read Al Sweigart's "Cracking Codes with Python".
 """
-
+from __future__ import annotations
 import argparse
 import os
 import sys
-import tempfile
-from typing import List, Dict
+# import tempfile
+from typing import List, Dict, Callable, Optional
+from enum import Enum, auto
 
+
+import cifra.cipher.affine
+import cifra.cipher.caesar
 import cifra.cipher.common
+import cifra.cipher.substitution
+import cifra.cipher.transposition
+import cifra.cipher.vigenere
 from cifra.attack.dictionaries import Dictionary
 
-if os.getenv("CIFRA_DEBUG", 0) == "1":
-    # TODO: Check Travis CI sets it corresponding CIFRA_DEBUG env var to 1.
-    DICTIONARY_FOLDER = tempfile.mkdtemp()
-else:
-    DICTIONARY_FOLDER = "~/.cifra/"
+# if os.getenv("CIFRA_DEBUG", 0) == "1":
+#     # TODO: Check Travis CI sets it corresponding CIFRA_DEBUG env var to 1.
+#     DICTIONARY_FOLDER = tempfile.mkdtemp()
+# else:
+#     DICTIONARY_FOLDER = "~/.cifra/"
 
-CIPHERING_ALGORITHMS = ["caesar", "substitution", "transposition", "affine",
-                        "vigenere"]
+
+class MessageOperation(Enum):
+    """ Possible operations over text messages (bot ciphered and plain)."""
+    cipher = auto()
+    decipher = auto()
+    attack = auto()
+
+    @staticmethod
+    def from_string(name: str) -> MessageOperation:
+        """ Create an MessageOperation enum from an operation name string.
+
+        :param name: Name string
+        :return: A message operation with that name.
+        """
+        if name == "cipher":
+            return MessageOperation.cipher
+        elif name == "decipher":
+            return MessageOperation.decipher
+        elif name == "attack":
+            return MessageOperation.attack
+
+
+class Algorithm(Enum):
+    """ Algorithms implemented in cifra."""
+    caesar = auto()
+    substitution = auto()
+    transposition = auto()
+    affine = auto()
+    vigenere = auto()
+
+    @staticmethod
+    def from_string(name: str) -> Algorithm:
+        """ Create an Algorithm enum from an algorithm name string.
+
+        :param name: Name string
+        :return: An algorithm with that name.
+        """
+        if name == "caesar":
+            return Algorithm.caesar
+        elif name == "substitution":
+            return Algorithm.substitution
+        elif name == "transposition":
+            return Algorithm.transposition
+        elif name == "affine":
+            return Algorithm.affine
+        elif name == "vigenere":
+            return Algorithm.vigenere
+
+
+# CIPHERING_ALGORITHMS = {"caesar", "substitution", "transposition", "affine",
+#                         "vigenere"}
+CIPHERING_ALGORITHMS = {key for key in Algorithm.__members__.keys()}
+
+STRING_KEY_ALGORITHMS = {"substitution", "vigenere"}
+
+INTEGER_KEY_ALGORITHMS = CIPHERING_ALGORITHMS - STRING_KEY_ALGORITHMS
+
+
+
 
 def _check_is_file(_string: str) -> str:
     """ Check this string really points to a valid file.
@@ -113,6 +177,11 @@ def parse_arguments(args: list = None) -> Dict[str, str]:
                                help="Path to output file to place ciphered text. If not used "
                                     "ciphered text will be dumped to console.",
                                metavar="OUTPUT_CIPHERED_FILE")
+    cipher_parser.add_argument("-c", "--charset",
+                               type=str,
+                               help=f"Default charset is: {cifra.cipher.common.DEFAULT_CHARSET}, but you can set here "
+                                    f"another.",
+                               metavar="CHARSET")
     # DECIPHERING MANAGEMENT
     decipher_parser = cifra_subparsers.add_parser(name="decipher",
                                                   help="Decipher a text using a key.")
@@ -134,6 +203,11 @@ def parse_arguments(args: list = None) -> Dict[str, str]:
                                  help="Path to output file to place deciphered text. If not used "
                                       "deciphered text will be dumped to console.",
                                  metavar="OUTPUT_DECIPHERED_FILE")
+    decipher_parser.add_argument("-c", "--charset",
+                               type=str,
+                               help=f"Default charset is: {cifra.cipher.common.DEFAULT_CHARSET}, but you can set here "
+                                    f"another.",
+                               metavar="CHARSET")
     # ATTACK MANAGEMENT
     attack_parser = cifra_subparsers.add_parser(name="attack",
                                                 help="Attack a ciphered text to get its key.")
@@ -163,22 +237,129 @@ def parse_arguments(args: list = None) -> Dict[str, str]:
     return filtered_parser_arguments
 
 
-def main(args: List[str] = sys.argv[1:]) -> None:
+def main(args=sys.argv[1:], database_path=None) -> None:
     arguments: Dict[str, str] = parse_arguments(args)
+
+    # DICTIONARY MANAGEMENT
     if arguments["mode"] == "dictionary":
         if arguments["action"] == "create":
             initial_words_file = arguments.get("initial_words_file", None)
-            with Dictionary.open(arguments["dictionary_name"], create=True, _database_path=DICTIONARY_FOLDER) as dictionary:
+            with Dictionary.open(arguments["dictionary_name"], create=True, _database_path=database_path) as dictionary:
                 if initial_words_file is not None:
                     dictionary.populate(initial_words_file)
-        if arguments["action"] == "delete":
-            Dictionary.remove_dictionary(arguments["dictionary_name"], _database_path=DICTIONARY_FOLDER)
-        if arguments["action"] == "update":
-            with Dictionary.open(arguments["dictionary_name"], create=False, _database_path=DICTIONARY_FOLDER) as dictionary:
+        elif arguments["action"] == "delete":
+            Dictionary.remove_dictionary(arguments["dictionary_name"], _database_path=database_path)
+        elif arguments["action"] == "update":
+            with Dictionary.open(arguments["dictionary_name"], create=False, _database_path=database_path) as dictionary:
                 dictionary.populate(arguments["words_file"])
+        elif arguments["action"] == "list":
+            dictionaries = Dictionary.get_available_languages(_database_path=database_path)
+            for dictionary in dictionaries:
+                print(dictionary)
+
+    # CIPHERING MANAGEMENT
+    elif arguments["mode"] == "cipher":
+        ciphered_content = _process_file_with_key(arguments["file_to_cipher"],
+                                                  Algorithm.from_string(arguments["algorithm"]),
+                                                  arguments["key"],
+                                                  MessageOperation.from_string(arguments["mode"]),
+                                                  arguments["charset"] if "charset" in arguments else None)
+        _output_result(ciphered_content, arguments)
+
+    # DECIPHERING MANAGEMENT
+    elif arguments["mode"] == "decipher":
+        deciphered_content = _process_file_with_key(arguments["file_to_decipher"],
+                                                  Algorithm.from_string(arguments["algorithm"]),
+                                                  arguments["key"],
+                                                  MessageOperation.from_string(arguments["mode"]),
+                                                  arguments["charset"] if "charset" in arguments else None)
+        _output_result(deciphered_content, arguments)
 
 
+def _output_result(result: str, arguments: Dict[str, str]) -> None:
+    """ Helper generic function to output resulting content.
+
+    :param result: String with resulting processed content. If an output file has been requested then result is
+    written to that file or to screen otherwise.
+    :param arguments: Console parsed arguments.
+    """
+    if ("ciphered_file" or "deciphered_file") in arguments:
+        output_filename = arguments["ciphered_file"] if "ciphered_file" in arguments else arguments["deciphered_file"]
+        with open(output_filename, mode="w") as output_file:
+            output_file.write(result)
+    else:
+        print(result)
+
+
+def _process_file_with_key(_input: str, algorithm: Algorithm, key: str,
+                           operation: MessageOperation, charset: str = None) -> str:
+    """ Helper generic function to process files to cipher and decipher.
+
+    :param _input: Input file pathname.
+    :param algorithm: Algorithm to apply for given operation.
+    :param key: Key to use with algorithm.
+    :param operation: Whether cipher, decipher or attack given input.
+    :param charset: Charset to use with given algorithm. If selected algorithm does not use a charset
+    then this is ignored.
+    :return: Processed resulting string.
+    """
+    with open(_input, mode="r") as input_file:
+        content_to_process = input_file.read()
+    process_function = _get_ciphering_function(algorithm) \
+        if operation == MessageOperation.cipher \
+        else _get_deciphering_function(algorithm)
+    process_key = key if algorithm.name in STRING_KEY_ALGORITHMS else int(key)
+    if charset is not None:
+        processed_content = process_function(content_to_process,
+                                             process_key,
+                                             charset)
+    else:
+        processed_content = process_function(content_to_process,
+                                             key)
+    return processed_content
+
+
+def _get_ciphering_function(algorithm: Algorithm) -> Callable:
+    """ Get ciphering function for this algorithm.
+
+    :param algorithm: Algorithm name.
+    :return: A ciphering function. None if function with that
+    name was not found.
+    """
+    ciphering_function = None
+    if algorithm == Algorithm.caesar:
+        ciphering_function = cifra.cipher.caesar.cipher
+    elif algorithm == Algorithm.affine:
+        ciphering_function = cifra.cipher.affine.cipher
+    elif algorithm == Algorithm.substitution:
+        ciphering_function = cifra.cipher.substitution.cipher
+    elif algorithm == Algorithm.transposition:
+        ciphering_function = cifra.cipher.transposition.cipher
+    elif algorithm == Algorithm.vigenere:
+        ciphering_function = cifra.cipher.vigenere.cipher
+    return ciphering_function
+
+
+def _get_deciphering_function(algorithm: Algorithm) -> Callable:
+    """ Get deciphering function for this algorithm.
+
+    :param algorithm: Algorithm name.
+    :return: A deciphering function. None if function with that
+    name was not found.
+    """
+    deciphering_function = None
+    if algorithm == Algorithm.caesar:
+        deciphering_function = cifra.cipher.caesar.decipher
+    elif algorithm == Algorithm.affine:
+        deciphering_function = cifra.cipher.affine.decipher
+    elif algorithm == Algorithm.substitution:
+        deciphering_function = cifra.cipher.substitution.decipher
+    elif algorithm == Algorithm.transposition:
+        deciphering_function = cifra.cipher.transposition.decipher
+    elif algorithm == Algorithm.vigenere:
+        deciphering_function = cifra.cipher.vigenere.decipher
+    return deciphering_function
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
